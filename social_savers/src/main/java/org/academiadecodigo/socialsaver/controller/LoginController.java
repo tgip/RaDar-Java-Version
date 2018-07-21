a54ccf9923f93cd326d803969d0e9cfcb80eeea5
Code Cadet <codecadet@babbage.local>
2018-07-21 03:16:53 +0000

    login controller working

--- social_savers/src/main/java/org/academiadecodigo/socialsaver/controller/LoginController.java
+++ social_savers/src/main/java/org/academiadecodigo/socialsaver/controller/LoginController.java
@@ -1,14 +1,38 @@
 package org.academiadecodigo.socialsaver.controller;
 
+import org.academiadecodigo.socialsaver.command.LoginForm;
+import org.academiadecodigo.socialsaver.services.DonerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.persistence.ManyToOne;
 
 @Controller
 public class LoginController {
 
+    private DonerService donerService;
+
+    @Autowired
+    public void setDonerService(DonerService donerService) {
+        this.donerService = donerService;
+    }
+
 	@RequestMapping(method = RequestMethod.GET, value = "/login")
-	public String root() {
+	public String root(Model model) {
+	    model.addAttribute("loginForm", new LoginForm());
 		return "login";
 	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/login")
+    public String login(@ModelAttribute LoginForm loginForm){
+        if(donerService.login(loginForm.getName(),loginForm.getPassword())){
+            return "Main";
+        }
+        return "login";
+    }
 }
